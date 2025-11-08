4443b747aa189fb0aea718c2a0cd487873e53f27
seriousdev-gh <borisdrovnin@gmail.com>
2025-11-08 15:14:19 +0300

    Initial commit

---
+++ lib/rubocop/dry/import.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "import/version"
+
+module Rubocop
+  module Dry
+    module Import
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/rubocop/dry/import_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Rubocop::Dry::Import do
+  it "has a version number" do
+    expect(Rubocop::Dry::Import::VERSION).not_to be nil
+  end
+end
